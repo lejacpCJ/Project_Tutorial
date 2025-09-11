237d07f51008d7224ac212b340f15542451ef61a
CJ Wang <lejacp@gmail.com>
2025-09-11 11:49:47 +0800

    Add Player and Enemy Character

---
+++ Source/Project_Tutorial/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_TUTORIAL_API AEnemyCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Project_Tutorial/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_TUTORIAL_API APlayerCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
